abc14f5ad51e9671a214d1f74f54b8748e9326a2
Marco Gagliardi <marcogagliardi84@gmail.com>
2019-10-29 17:23:04 +0100

    refactor

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,50 @@
 const path = require('path')
 const bundleOutputDir = './dist';
-var copyWebpackPlugin = require('copy-webpack-plugin');
+let copyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, bundleOutputDir),
-    publicPath: '/'
-  },
-  devServer: {
-    historyApiFallback: true,
-    disableHostCheck: true,
-    contentBase: bundleOutputDir
-  },
-  plugins: [
-    new copyWebpackPlugin([{ from: 'public/' }])
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: [['@babel/env']]
-          }
-        }
-      },
-      { test: /\.html$/i, use: 'html-loader' },
-      {
-        test: /\.(jpg|png|gif|svg|pdf|ico)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[path][name]-[hash:8].[ext]'
+    entry: './src/index.js',
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, bundleOutputDir),
+        publicPath: '/'
+    },
+    devServer: {
+        historyApiFallback: true,
+        disableHostCheck: true,
+        contentBase: bundleOutputDir
+    },
+    plugins: [
+        new copyWebpackPlugin([{from: 'public/'}])
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [['@babel/env']]
+                    }
+                }
             },
-          },
+            {test: /\.html$/i, use: 'html-loader'},
+            {
+                test: /\.(jpg|png|gif|svg|pdf|ico)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[path][name]-[hash:8].[ext]'
+                        },
+                    },
+                ]
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            }
         ]
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      }
-    ]
-  }
+    }
 };
