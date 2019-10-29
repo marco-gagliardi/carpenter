b4d05da439b3e35bed894ebe028631d74a4bb95d
Marco Gagliardi <marcogagliardi84@gmail.com>
2019-10-29 17:25:58 +0100

    fix indents

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { StrictMode } from 'react'
+import React, {StrictMode} from 'react'
 import Routes from "./pages/Routes";
-import { Provider } from 'react-redux'
+import {Provider} from 'react-redux'
 import store from './stores'
 import ErrorHandler from './components/ErrorHandler'
 import './App.css'
--- webpack.config.js
+++ webpack.config.js
@@ -3,48 +3,48 @@ const bundleOutputDir = './dist';
 let copyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, bundleOutputDir),
-        publicPath: '/'
-    },
-    devServer: {
-        historyApiFallback: true,
-        disableHostCheck: true,
-        contentBase: bundleOutputDir
-    },
-    plugins: [
-        new copyWebpackPlugin([{from: 'public/'}])
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: [['@babel/env']]
-                    }
-                }
+  entry: './src/index.js',
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, bundleOutputDir),
+    publicPath: '/'
+  },
+  devServer: {
+    historyApiFallback: true,
+    disableHostCheck: true,
+    contentBase: bundleOutputDir
+  },
+  plugins: [
+    new copyWebpackPlugin([{from: 'public/'}])
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [['@babel/env']]
+          }
+        }
+      },
+      {test: /\.html$/i, use: 'html-loader'},
+      {
+        test: /\.(jpg|png|gif|svg|pdf|ico)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name]-[hash:8].[ext]'
             },
-            {test: /\.html$/i, use: 'html-loader'},
-            {
-                test: /\.(jpg|png|gif|svg|pdf|ico)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[path][name]-[hash:8].[ext]'
-                        },
-                    },
-                ]
-            },
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            }
+          },
         ]
-    }
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      }
+    ]
+  }
 };
