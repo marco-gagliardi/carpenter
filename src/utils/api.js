69847e4c5cec25322d7d82731968d713027dcdd8
Marco Gagliardi <marcogagliardi84@gmail.com>
2019-11-10 02:53:52 +0100

    simplify function

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,17 +4,14 @@ import axios from 'axios'
 const BASE_URL = config.api.baseUrl;
 const DEFAULT_PARAMS = config.api.defaultParams;
 const DEFAULT_OPTIONS = { headers: {'Content-Type': 'application/json'} };
-const buildQueryString = (obj = {}) => {
-  if (typeof obj !== 'object' || Object.keys(obj).length === 0) {
-    return ''
-  }
-  return '?' + Object.keys(obj).map(key => `${key}=${obj[key]}`).join('&')
-};
+
 const makeRequest = (method = 'get', endpoint, options = {}) => {
-  const {params, ...rest} = options;
-  const queryStringParams = {...DEFAULT_PARAMS, ...params};
-  const url = `${BASE_URL}/${endpoint}${buildQueryString(queryStringParams)}`;
-  options = {...DEFAULT_OPTIONS, ...rest};
+  options = {
+    ...DEFAULT_OPTIONS,
+    ...options,
+    params: {...DEFAULT_PARAMS, ...options.params}
+  }
+  const url = `${BASE_URL}/${endpoint}`;
   return axios({url, method, ...options})
     .then(response => response.data)
     .catch(response => {
